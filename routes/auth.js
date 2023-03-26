43f002d3c31f31a61415b31e9227b1040ff1481e
JulianVilloria <julian@polluxcoop.com>
2023-03-25 21:54:48 -0300

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,32 @@ const bcrypt = require("bcryptjs");
 const SECRET_KEY = "secretkey123456";
 
 router.post("/register", function (req, res, next) {
+  console.log("sadsadsadsa");
+  const newPlayer = {
+    nombre_usuario: req.body.name,
+    password: bcrypt.hashSync(req.body.password),
+  };
+
+  Player.create(newPlayer, (err, player) => {
+    if (err && err.code === 11000)
+      return res.status(409).send("Email already exists");
+    if (err) return res.status(500).send("Server error");
+    const expiresIn = 24 * 60 * 60;
+    const accessToken = jwt.sign({ id: player.id }, SECRET_KEY, {
+      expiresIn: expiresIn,
+    });
+    const playerData = {
+      username: Player.name,
+      password: Player.password,
+      accessToken: accessToken,
+      expiresIn: expiresIn,
+    };
+    // response
+    res.send({ playerData });
+  });
+});
+
+router.post("/login", function (req, res, next) {
   const PlayerData = {
     username: req.body.username,
     password: req.body.password,
@@ -37,31 +63,6 @@ router.post("/register", function (req, res, next) {
         res.status(409).send({ message: "Something is wrong" });
       }
     }
-  });
-});
-router.post("/login", function (req, res, next) {
-  console.log("sadsadsadsa");
-  const newPlayer = {
-    nombre_usuario: req.body.name,
-    password: bcrypt.hashSync(req.body.password),
-  };
-
-  Player.create(newPlayer, (err, player) => {
-    if (err && err.code === 11000)
-      return res.status(409).send("Email already exists");
-    if (err) return res.status(500).send("Server error");
-    const expiresIn = 24 * 60 * 60;
-    const accessToken = jwt.sign({ id: player.id }, SECRET_KEY, {
-      expiresIn: expiresIn,
-    });
-    const playerData = {
-      username: Player.name,
-      password: Player.password,
-      accessToken: accessToken,
-      expiresIn: expiresIn,
-    };
-    // response
-    res.send({ playerData });
   });
 });
 
